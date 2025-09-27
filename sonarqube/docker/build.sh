11b87fe6441f12ed4e0cd3d668bfc95c27c9671c
xPlorinRolyPoly <alpanachaphalkar@gmail.com>
2025-09-27 14:46:40 +0200

    Update GCP bucket variable for SonarQube plugin paths

--- sonarqube/docker/build.sh
+++ sonarqube/docker/build.sh
@@ -7,6 +7,7 @@ colima start --cpu 4 --memory 16 --network-address
 export PROJECT_SHORT_NAME="nb"
 export PROJECT_ID=<YOUR_PROJECT_ID>
 export ARTIFACT_REGISTRY_HOST=<YOUR_ARTIFACT_REGISTRY_HOST>
+export GCP_BUCKET_SQ_PLUGINS="gs://<YOUR_GCP_BUCKET_NAME>"
 
 gcloud config configurations activate <YOUR_GCLOUD_CONFIG_PROFILE>
 gcloud auth configure-docker $ARTIFACT_REGISTRY_HOST
@@ -22,7 +23,7 @@ DOCKER_BUILDKIT=1 docker buildx build \
   --build-arg SONARQUBE_VERSION="2025.1.1.104738" \
   --build-arg "SONARQUBE_ZIP_URL=https://binaries.sonarsource.com/CommercialDistribution/sonarqube-developer/sonarqube-developer-2025.1.1.104738.zip" \
   --build-arg "GCP_PROJECT_ID=$PROJECT_ID" \
-  --build-arg "SONARQUBE_PLUGIN_GCS_PATHS=gs://de-nc-st-bu-docker-image-addins/sonar-salesforce-plugin-24.0.13.jar gs://de-nc-st-bu-docker-image-addins/sonar-codescanlang-plugin-24.0.13.jar" \
+  --build-arg "SONARQUBE_PLUGIN_GCS_PATHS=$GCP_BUCKET_SQ_PLUGINS/sonar-salesforce-plugin-24.0.13.jar $GCP_BUCKET_SQ_PLUGINS/sonar-codescanlang-plugin-24.0.13.jar" \
   --secret id=gcp_sa_key,src=$HOME/.config/gcloud/nc/global/sa.json \
   -t $ARTIFACT_REGISTRY_HOST/$PROJECT_ID/syst-2374/sonarqube:2025.1.1.104738-developer .
 
